8036c69582fcc6b1d44152e165fc6618b28627af
ASUS73 <114318674+ASUS73@users.noreply.github.com>
2024-03-31 16:40:42 +0700

    Add files via upload

---
+++ WebsiteSederhana/footer.php
@@ -0,0 +1,9 @@
+<footer>
+    <div class="container">
+        <div class="row">
+          	<div class="col-sm-12">
+            	<p class="text-white text-center">&copy;Copyright 2024 | built by Ammar Yusron Abhinaya</p>
+          	</div>
+        </div>
+      </div>
+</footer>
